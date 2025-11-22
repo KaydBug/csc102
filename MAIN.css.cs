ca36a330b8552ac375f89a816ca098ea548b8e3d
KaydBug <kaydmae94@gmail.com>
2025-11-22 17:02:37 -0600

    homework for november 23
    i think i got it right

---
+++ MAIN.css.cs
@@ -0,0 +1,42 @@
+*/
+
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    margin: 0;
+    padding: 16px;
+    background: #fafafa;
+    color: #222;
+}
+
+/* Color / utility classes */
+.green-bg { background-color: #4CAF50; }
+.blue-bg  { background-color: #3498db; }
+.grey-text { color: grey; }
+.red-text { color: #c0392b; }
+
+/* Navigation */
+nav{
+    height: 400px;
+    width: 100px;
+    float: left;
+    padding: 5px;
+    line-height: 30px;
+}
+
+/* Footer */
+footer{
+    text-align: center;
+    margin-right: 100px;
+}
+
+/* Image with id="memeImage" */
+<img id="How-to-Draw-Ruby-Bridges-web.jpg"> 
+    display: block;
+    margin: 0 auto 16px auto;
+    width: 400px;
+    max-width: 100%;
+}
+
+h1{
+    text-align: center;
+}
